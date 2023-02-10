119762bba8d7082955430525c8327378f6800c14
yoyo-1487 <yoyotsai0928@gmail.com>
2023-02-10 15:44:34 +0800

    init project

--- src/main/java/com/example/menu/aggregate/MenuAggregate.java
+++ src/main/java/com/example/menu/aggregate/MenuAggregate.java
@@ -1,9 +1,9 @@
 package com.example.menu.aggregate;
 
-import com.example.menu.dao.MenuDao;
-import com.example.menu.dao.OrderDao;
-import com.example.menu.entity.MenuEntity;
-import com.example.menu.entity.OrderEntity;
+import com.example.menu.model.dao.MenuDao;
+import com.example.menu.model.dao.OrderDao;
+import com.example.menu.model.entity.MenuEntity;
+import com.example.menu.model.entity.OrderEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -12,37 +12,37 @@ import java.util.UUID;
 
 @Repository
 public class MenuAggregate {
-    private int Totalprice=0;
+    private int totalPrice=0;
     @Autowired
     MenuDao menuDao;
     @Autowired
     OrderDao orderDao;
     //------------查詢菜單------------
-    public Iterable<MenuEntity> getMenu_all() {
+    public Iterable<MenuEntity> getMenuAll() {
         return menuDao.findAll();
     }
     //------------接值------------
-    public Optional<MenuEntity> find_and_order (String item){
+    public Optional<MenuEntity> findMenuByItem (String item){
         return menuDao.findById(item);
     }
     //------------判斷品項存在並加入------------
-    public String find_and_order_exist (Optional<MenuEntity> item){
+    public String existThenOrder (Optional<MenuEntity> item){
         if(item.isEmpty()){
             return "不存在";
         }
         else{
             OrderEntity order = new OrderEntity();
-            order.setOrder_id(UUID.randomUUID().toString());
-            order.setOrder_item(item.get().getItems());
-            order.setOrder_price(item.get().getPrice());
+            order.setOrderId(UUID.randomUUID().toString());
+            order.setOrderItem(item.get().getItems());
+            order.setOrderPrice(item.get().getPrice());
             orderDao.save(order);//-----將值寫入table_menu
-            Totalprice = Totalprice + item.get().getPrice();//-----加總金額
+            totalPrice = totalPrice + item.get().getPrice();//-----加總金額
             return "成功點餐,餐點為:" + item.get().getItems() + ",價格:" + item.get().getPrice();
         }
     }
     //------------加總金額------------
-    public Integer totalprice(){
-        return Totalprice;
+    public Integer totalPrice(){
+        return totalPrice;
     }
     //--------------------------------------------------------
 }
--- src/main/java/com/example/menu/model/dao/MenuDao.java
+++ src/main/java/com/example/menu/model/dao/MenuDao.java
@@ -1,8 +1,7 @@
-package com.example.menu.dao;
+package com.example.menu.model.dao;
 
-import com.example.menu.entity.MenuEntity;
+import com.example.menu.model.entity.MenuEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 // 第一個參數為訪問的實體，第二參數是這個Entity ID的資料型態
 
